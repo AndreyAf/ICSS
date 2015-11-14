878f1b4878a69e3d515f576260d25201d1e9dedd
AndreyAf <andreyp@welldone-software.com>
2015-11-14 21:47:49 +0200

    Fixes model list export lib

--- packages/core/articles/public/controllers/articles.js
+++ packages/core/articles/public/controllers/articles.js
@@ -183,7 +183,26 @@ angular.module('mean.' + modelName.plural.toLocaleLowerCase()).controller(modelN
                     '   </a>' +
                     '</div>'
                 }
-            ]
+            ],
+            enableGridMenu: true,
+            enableSelectAll: true,
+            exporterCsvFilename: $scope.modelName.plural+Date.now().toString()+'.csv',
+            exporterPdfDefaultStyle: {fontSize: 9},
+            exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+            exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+            exporterPdfHeader: { text: $scope.modelName.plural, style: 'headerStyle' },
+            exporterPdfFooter: function ( currentPage, pageCount ) {
+                return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+            },
+            exporterPdfCustomFormatter: function ( docDefinition ) {
+                docDefinition.styles.headerStyle = { fontSize: 22, bold: true };
+                docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+                return docDefinition;
+            },
+            exporterPdfOrientation: 'portrait',
+            exporterPdfPageSize: 'LETTER',
+            exporterPdfMaxGridWidth: 500,
+            exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location"))
         };
 
         // TODO: rewrite
